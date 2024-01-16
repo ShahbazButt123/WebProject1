39897d6d4bf0bb59b714df5b43291dcb054785f5
ShahbazButt123 <shahbazbutt4354@gmail.com>
2024-01-16 21:38:40 +0500

    update

---
+++ Weather and News/Models/OpenWeatherResponse.cs
@@ -0,0 +1,29 @@
+﻿namespace Weather_and_News.Models
+{
+    public class OpenWeatherResponse
+    {
+        public string Name { get; set; } // City name
+        public MainData Main { get; set; }
+        public List<WeatherData> Weather { get; set; }
+        public WindData Wind { get; set; } 
+        public int Humidity { get; set; } 
+        public int Pressure { get; set; } 
+    }
+
+    public class MainData
+    {
+        public float Temp { get; set; } 
+        public int Humidity { get; set; } 
+        public int Pressure { get; set; } 
+    }
+
+    public class WeatherData
+    {
+        public string Description { get; set; } 
+    }
+
+    public class WindData
+    {
+        public float Speed { get; set; } 
+    }
+}
---
+++ Weather and News/Models/WeatherInfo.cs
@@ -0,0 +1,23 @@
+﻿namespace Weather_and_News.Models
+{
+    public class WeatherInfo
+    {
+        public string City { get; set; }
+        public string Temperature { get; set; }
+        public string Description { get; set; }
+        public string Humidity { get; set; }
+        public string WindSpeed { get; set; }
+
+        public string Pressure { get; set; }
+    }
+    public class NewsArticle
+    {
+        public string Title { get; set; }
+        public string Link { get; set; }
+        public string Description { get; set; }
+        public string ImageUrl { get; set; }
+        public string Text { get; set; }
+    }
+
+
+}
